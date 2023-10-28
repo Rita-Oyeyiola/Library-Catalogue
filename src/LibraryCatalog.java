3c9e0fb5d92631591bbf8fa63e0f70a567745f1f
Rita <oyeyiolarita221@gmail.com>
2023-10-28 19:22:41 +0100

    updated main.java and libraryCatalog class

---
+++ src/LibraryCatalog.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class LibraryCatalog {
+    public ArrayList<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(String title) {
+        Book book = findBook(title);
+        if (book == null){
+            System.out.println("The book with"+ title + "does not exist");
+            return;
+        }
+        books.remove(book);
+    }
+
+    public Book findBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
+
+        return null;
+    }
+
+
+    public void listBooks() {
+        for (Book book : books) {
+            System.out.println("Title" + book.getTitle());
+            System.out.println("Author" + book.getAuthor());
+            System.out.println("ISBN" + book.getISBN());
+            System.out.println("Genre" + book.getGenre());
+            System.out.println();
+        }
+    }
+
+
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,80 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    Book books = new Book();
 
         Scanner scanner = new Scanner(System.in);
+        LibraryCatalog libraryCatalog = new LibraryCatalog();
 
-        System.out.println("Please enter the author of the book");
-        String author = scanner.nextLine();
-        books.setAuthor(author);
+        while (true){
+            System.out.println("Library Catalog Management System");
+            System.out.println("1. Add Book");
+            System.out.println("2. Remove Book");
+            System.out.println("3. Find Book");
+            System.out.println("4. List Books");
+            System.out.println("5. Exit");
+            System.out.println("Enter your Choice:");
 
-        System.out.println("Please enter the title of the book");
-        String title = scanner.nextLine();
-        books.setTitle(title);
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Please enter the ISBN of the book");
-        String ISBN = scanner.nextLine();
-        books.setISBN(ISBN);
+            switch (choice){
+                case 1:
+                    Book book = new Book();
+                    System.out.println("Please enter the title of the book /n");
+                    String title = scanner.nextLine();
+                    book.setTitle(title);
 
-        System.out.println("Please enter genre");
-        String genre = scanner.nextLine();
-        books.setGenre(genre);
+                    System.out.println("Please enter the author of the book /n");
+                    String author = scanner.nextLine();
+                    book.setAuthor(author);
+
+                    System.out.println("Please enter the isbn of the book /n");
+                    String isbn = scanner.nextLine();
+                    book.setISBN(isbn);
+
+                    System.out.println("Please enter the genre of the book /n");
+                    String genre = scanner.nextLine();
+                    book.setISBN(genre);
+
+                    libraryCatalog.addBook(book);
+                    break;
+
+                case 2:
+                    System.out.println("Please enter the title of the book you want to remove");
+                    String bookTitle = scanner.nextLine();
+                    libraryCatalog.removeBook(bookTitle);
+                    break;
+
+                case 3:
+                    System.out.println("Please enter the title of the book you want to find/n");
+                    String bookName = scanner.nextLine();
+                    Book bookInfo = libraryCatalog.findBook(bookName);
+
+                    System.out.println("Title:" + bookInfo.getTitle());
+                    System.out.println("Author:" + bookInfo.getAuthor());
+                    System.out.println("ISBN:" + bookInfo.getISBN());
+                    System.out.println("Genre:" + bookInfo.getGenre());
+                    System.out.println();
+                    break;
+
+                case 4:
+                    System.out.println();
+                    libraryCatalog.listBooks();
+                    break;
+
+                case 5:
+                    System.out.println("Exiting. Thank you!");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again./n");
+                    break;
 
 
-        ArrayList<String> LibraryBooks =
-               new ArrayList<>(Arrays.asList("science","math","english","political science"));
 
-        LibraryCatalogue libraryAccess = new LibraryCatalogue();
-        System.out.println(LibraryBooks);
+            }
 
-
+        }
 
     }
 }
